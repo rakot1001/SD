0f5bd484bd014e9469c194b27fd0bbf9efe322d5
Vlad <vladdp70@gmail.com>
2021-06-13 18:21:46 +0300

    Прошлые задания

--- js/index.js
+++ js/index.js
@@ -1,3 +1,57 @@
-'use strict'
+"use strict";
 
+/* Создать массив из 10 чисел. Числа 0,2,4,6,8 и т.д.
+Двумя разными циклами перебрать масисив, увеличив каждый элемент на 1 */
 
+const arr = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18];
+
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i]++;
+//   console.log(arr[i]);
+// }
+
+// arr.forEach(arr=>{
+//   console.log(arr);
+// })
+
+// for (let ar of arr) {
+//   ar += 1;
+//   console.log(ar);
+// }
+
+/* Всплывающее окно. В нем нужно ввести число. Второе всплувающее окно. Вводим слово. К этому слову нужно дописать в конец столько
+воскл. знаков, сколько в этом числе
+4
+abc
+abc!!!!*/
+
+// const input1 = +prompt("Введите число");
+// const input2 = prompt("Введите слово:");
+// let inp = input2;
+
+// function getExpr(input1, inp) {
+//   if (input1.isNaN()) {
+//     throw new TypeError("Not a number");
+//   }
+//   const expr = "!";
+//   for (let i = 0; i < input1; i++) {
+//     inp += expr;
+//   }
+//   return;
+// }
+// const res = getExpr(input1, inp);
+// console.log(res);
+/*Создать мапу, в которой ключ - название города
+, а значение - численность населения млн
+Найти город с самым численным населением*/
+
+// const map = new Map();
+// map.set("Днепр", 10000);
+// map.set("Запорожье", 12000);
+// map.set("Львов", 11000);
+
+// for (const [city, value] of cities) {
+//   if(`${value}`)
+// }
+
+event=>node=>myArray
